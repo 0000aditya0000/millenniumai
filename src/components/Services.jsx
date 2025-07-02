0bca03c8545686c0170a97a626f071473919a489
Pratham Gupta <guptapratham661@gmail.com>
2025-07-02 21:48:45 +0530

    Refactor Services component by removing unused image imports and client data to improve code clarity and maintainability.

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,24 +1,4 @@
 import { FaBoxOpen, FaTruck, FaCheckCircle, FaPalette, FaCar, FaEraser, FaTags, FaCogs, FaBolt, FaShieldAlt } from "react-icons/fa";
-import browntape from "../assets/42_files/powerfixit.pdf-image-004.jpg"
-import colurtape from "../assets/42_files/powerfixit.pdf-image-005.jpg"
-import dormewear from "../assets/42_files/powerfixit.pdf-image-032.jpg"
-import redrock from "../assets/42_files/powerfixit.pdf-image-033.png"
-import kakkar from "../assets/42_files/powerfixit.pdf-image-035.jpg"
-import powerfixit from "../assets/42_files/powerfixit.pdf-image-036.jpg"        
-import hyeto from "../assets/42_files/powerfixit.pdf-image-037.jpg"
-import ciel from "../assets/42_files/powerfixit.pdf-image-043.jpg"
-import vaikunth from "../assets/42_files/powerfixit.pdf-image-039.jpg"
-import snapdo from "../assets/42_files/powerfixit.pdf-image-048.jpg"
-import swadhin from "../assets/42_files/powerfixit.pdf-image-037.jpg"
-import bhopa from "../assets/42_files/powerfixit.pdf-image-042.jpg"
-import pearl from "../assets/42_files/powerfixit.pdf-image-006.png" 
-import krishna from "../assets/42_files/powerfixit.pdf-image-047.jpg"
-import pharmacy from "../assets/42_files/powerfixit.pdf-image-052.jpg"
-import yellow from "../assets/42_files/powerfixit.pdf-image-045.png"
-import zcpi from "../assets/42_files/powerfixit.pdf-image-053.jpg"
-import masking from "../assets/42_files/powerfixit.pdf-image-006.png"
-import custom from "../assets/42_files/powerfixit.pdf-image-015.png"
-import { motion } from "framer-motion";
 
 const services = [
   {
@@ -77,31 +57,8 @@ const services = [
   },
 ];
 
-const printedTapeClients = [
-  { name: "Dormewear", img: dormewear },
-  { name: "Red Rock", img: redrock },
-  { name: "Kakkar", img: kakkar },
-  { name: "Power Fixit", img: powerfixit },
-  { name: "Hyeto", img: hyeto },
-  { name: "Ciel Sports", img: ciel },
-  { name: "Vaikunth", img: vaikunth },
-  { name: "Snapdo", img: snapdo },
-  { name: "Swadhin Biri", img: swadhin },
-  { name: "Bhopa", img: bhopa },
-  { name: "Pearl", img: pearl },
-  { name: "Krishna", img: krishna },
-  { name: "Pharmacy Healthcare", img: pharmacy },
-  { name: "Yellow", img: yellow },
-  { name: "ZCPI", img: zcpi }
-];
 
-const productTable = [
-  { brand: "POWER FIXIT", length: "25 to 650 mtr.", colour: "Clear", core: "Inner", thickness: "36", size: "12", jumbo: "144" },
-  { brand: "WONDER BRO", length: "Body Jumbo Length", colour: "Red, Yellow", core: "Diameter", thickness: "38", size: "48", jumbo: "210" },
-  { brand: "MARUTI GOLD", length: "1500 mtr.", colour: "Green", core: "Oil core", thickness: "42", size: "36", jumbo: "246" },
-  { brand: "SHREE GEE", length: "Jumbo Length 1025/1315", colour: "Yellow", core: "76 mm", thickness: "48", size: "48", jumbo: "280" },
-  { brand: "POWER GRIP", length: "4000 mtr.", colour: "Blue", core: "76 mm", thickness: "55", size: "72", jumbo: "1315" }
-];
+
 
 export default function Services() {
   return (
